46e43cc94ebba2167ef00ca29db9db7d8f7a25a7
Daniel Shupe <dlshupe@gmail.com>
2021-04-25 19:17:24 -0400

    commit

---
+++ Models/DisplayEncounterModel.cs
@@ -0,0 +1,20 @@
+﻿using PF2eDataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PF2e_Encounter_Builder.Models
+{
+    public class DisplayEncounterModel
+    {
+        public string DifficultyId;
+        public int TypeId;
+        public string TypeName;
+        public int TerrainId;
+        public string TerrainName;
+        public int Level;
+        public List<MonsterModel> Monsters;
+        public int Players;
+    }
+}
---
+++ Models/DisplayMonsterModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PF2e_Encounter_Builder.Models
+{
+    public class DisplayMonsterModel
+    {
+        [Required]
+        public string MonsterName;
+        [Required]
+        [Range(-1,25)]
+        public int Level;
+        public int FamilyId;
+        public int TerrainId;
+        public int TypeId;
+        public string FamilyName;
+        public string TypeName;
+        public string TerrainName;
+    }
+}
