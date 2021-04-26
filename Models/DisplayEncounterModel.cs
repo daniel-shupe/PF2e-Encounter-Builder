2434fb272424d323766ef937c6c5f4c21fc1cfae
Daniel Shupe <dlshupe@gmail.com>
2021-04-25 22:06:54 -0400

    Updated comments

--- Models/DisplayEncounterModel.cs
+++ Models/DisplayEncounterModel.cs
@@ -6,15 +6,16 @@ using System.Threading.Tasks;
 
 namespace PF2e_Encounter_Builder.Models
 {
+    //  model used to hold parameters for encounter generation
     public class DisplayEncounterModel
     {
-        public string DifficultyId;
-        public int TypeId;
-        public string TypeName;
-        public int TerrainId;
-        public string TerrainName;
-        public int Level;
-        public List<MonsterModel> Monsters;
-        public int Players;
+        public string Difficulty;   //  difficulty of random encounter
+        public string CreatureType; //  creature type of random encounter
+        public string Terrain;  //  terrain of random encounter
+        public int Level;   //  average level of party
+        public List<MonsterModel> Monsters; //  list of available monsters
+        public int Players; //  party size
+        public string Alignment;    //  alignment of monsters in encounter
+        public string Family;   //  family of monsters in encounter
     }
 }
